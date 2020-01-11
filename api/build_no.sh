b2293192de9112fc9c1cac4e93105c99b1a86101
Uwe McVoid <eris@uv-software.de>
2020-01-11 14:45:20 +0100

    [MACCAN-24] Updated the build number generator script

--- api/build_no.sh
+++ api/build_no.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
-echo "/*  -- Do not commit this file --" > can_vers.h
-echo " *" >> can_vers.h
-echo " *  project   :  CAN - Controller Area Network" >> can_vers.h
-echo " *" >> can_vers.h
-echo " *  purpose   :  CAN Interface API, Version 3 (PCBUSB)" >> can_vers.h
-echo " *" >> can_vers.h
-echo " *  copyright :  (C) 2019-2020, UV Software, Berlin" >> can_vers.h
-echo " *" >> can_vers.h
-echo " *  compiler  :  Apple clang version 11.0.0 (clang-1100.0.33.16)" >> can_vers.h
-echo " *" >> can_vers.h
-echo " *  export    :  BUILD_NO" >> can_vers.h
-echo " *" >> can_vers.h
-echo " *  includes  :  (none)" >> can_vers.h
-echo " *" >> can_vers.h
-echo " *  author    :  Uwe Vogt, UV Software" >> can_vers.h
-echo " *" >> can_vers.h
-echo " *  e-mail    :  uwe.vogt@uv-software.de" >> can_vers.h
-echo " *" >> can_vers.h
-echo " */" >> can_vers.h
-echo "#ifndef CAN_VERS_H_INCLUDED" >> can_vers.h
-echo "#define CAN_VERS_H_INCLUDED" >> can_vers.h
-echo "#define BUILD_NO 0x"$(git log -1 --pretty=format:%h) >> can_vers.h
-echo "#define STRINGIFY(X) #X" >> can_vers.h
-echo "#define TOSTRING(X) STRINGIFY(X)" >> can_vers.h
-echo "#define SVN_REV_INT (BUILD_NO)" >> can_vers.h
-echo "#define SVN_REV_STR TOSTRING(BUILD_NO)" >> can_vers.h
-echo "#endif" >> can_vers.h
-echo "/*  ----------------------------------------------------------------------" >> can_vers.h
-echo " *  Uwe Vogt,  UV Software,  Chausseestrasse 33 A,  10115 Berlin,  Germany" >> can_vers.h
-echo " *  Tel.: +49-30-46799872,  Fax: +49-30-46799873,  Mobile: +49-170-3801903" >> can_vers.h
-echo " *  E-Mail: uwe.vogt@uv-software.de,  Homepage: http://www.uv-software.de/" >> can_vers.h
-echo " */" >> can_vers.h
+echo "/*  -- Do not commit this file --" > build_no.h
+echo " *" >> build_no.h
+echo " *  project   :  CAN - Controller Area Network" >> build_no.h
+echo " *" >> build_no.h
+echo " *  purpose   :  CAN Interface API, Version 3 (PCBUSB)" >> build_no.h
+echo " *" >> build_no.h
+echo " *  copyright :  (C) 2019-2020, UV Software, Berlin" >> build_no.h
+echo " *" >> build_no.h
+echo " *  compiler  :  Apple clang version 11.0.0 (clang-1100.0.33.16)" >> build_no.h
+echo " *" >> build_no.h
+echo " *  export    :  BUILD_NO" >> build_no.h
+echo " *" >> build_no.h
+echo " *  includes  :  (none)" >> build_no.h
+echo " *" >> build_no.h
+echo " *  author    :  Uwe Vogt, UV Software" >> build_no.h
+echo " *" >> build_no.h
+echo " *  e-mail    :  uwe.vogt@uv-software.de" >> build_no.h
+echo " *" >> build_no.h
+echo " */" >> build_no.h
+echo "#ifndef BUILD_NO_H_INCLUDED" >> build_no.h
+echo "#define BUILD_NO_H_INCLUDED" >> build_no.h
+echo "#define BUILD_NO 0x"$(git log -1 --pretty=format:%h) >> build_no.h
+echo "#define STRINGIFY(X) #X" >> build_no.h
+echo "#define TOSTRING(X) STRINGIFY(X)" >> build_no.h
+echo "#define SVN_REV_INT (BUILD_NO)" >> build_no.h
+echo "#define SVN_REV_STR TOSTRING(BUILD_NO)" >> build_no.h
+echo "#endif" >> build_no.h
+echo "/*  ----------------------------------------------------------------------" >> build_no.h
+echo " *  Uwe Vogt,  UV Software,  Chausseestrasse 33 A,  10115 Berlin,  Germany" >> build_no.h
+echo " *  Tel.: +49-30-46799872,  Fax: +49-30-46799873,  Mobile: +49-170-3801903" >> build_no.h
+echo " *  E-Mail: uwe.vogt@uv-software.de,  Homepage: http://www.uv-software.de/" >> build_no.h
+echo " */" >> build_no.h
