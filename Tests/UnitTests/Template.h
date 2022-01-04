edf65d3a7e4b8ac13f8fcd097bbaef7882ab94a6
Uwe McVogt <mac@uv-software.de>
2022-01-04 19:52:36 +0100

    Restore testing template for this project

--- Tests/UnitTests/Template.h
+++ Tests/UnitTests/Template.h
@@ -1,12 +1,12 @@
 //
-//  <project> - macOS User-Space Driver for <interface>
+//  PeakCAN - macOS User-Space Driver for Peak-System USB-to-CAN Interfaces
 //
 //  Copyright (C) 2021  Uwe Vogt, UV Software, Berlin (info@mac-can.com)
 //  All rights reserved.
 //
-//  This file is part of <project>.
+//  This file is part of MacCAN-PeakCAN.
 //
-//  <project> is dual-licensed under the BSD 2-Clause "Simplified" License and
+//  MacCAN-PeakCAN is dual-licensed under the BSD 2-Clause "Simplified" License and
 //  under the GNU General Public License v3.0 (or any later version).
 //  You can choose between one of them if you use this file.
 //
@@ -19,7 +19,7 @@
 //     this list of conditions and the following disclaimer in the documentation
 //     and/or other materials provided with the distribution.
 //
-//  <project> IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  MacCAN-PeakCAN IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 //  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 //  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 //  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
@@ -28,21 +28,21 @@
 //  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 //  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 //  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-//  OF <project>, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  OF MacCAN-PeakCAN, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //  GNU General Public License v3.0 or later:
-//  <project> is free software: you can redistribute it and/or modify
+//  MacCAN-PeakCAN is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
-//  <project> is distributed in the hope that it will be useful,
+//  MacCAN-PeakCAN is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with <project>.  If not, see <http://www.gnu.org/licenses/>.
+//  along with MacCAN-PeakCAN.  If not, see <http://www.gnu.org/licenses/>.
 //
 #ifndef TEMPLATE_H_INCLUDED
 #define TEMPLATE_H_INCLUDED
@@ -50,43 +50,50 @@
 //  A Template for CAN API V3 Testing
 //
 //  (§1) include the header file of the CAN API V3 C++ class of the CAN driver
-#include "<class>.h"
+#include "PeakCAN.h"
 
 //  (§2) define macro CDriverCAN with the class name of the CAN driver
-#define CDriverCAN  C<class>
+#define CDriverCAN  CPeakCAN
 
 //  (§3) define macro CAN_DEVICE1 and CAN_DEVICE2 with a valid CAN channel no.
-#define CAN_DEVICE1  <channel>0
-#define CAN_DEVICE2  <channel>1
+#define CAN_DEVICE1  PCAN_USB1
+#define CAN_DEVICE2  PCAN_USB2
 
 //  (§4) define macros for CAN Classic bit-rate settings (at least BITRATE_1M, BITRATE_500K, BITRATE_250K, BITRATE_125K)
-#define BITRATE_1M(x)       do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; } while(0)
-#define BITRATE_500K(x)     do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; } while(0)
-#define BITRATE_250K(x)     do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; } while(0)
-#define BITRATE_125K(x)     do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; } while(0)
+#define BITRATE_1M(x)    do {x.btr.frequency=8000000;x.btr.nominal.brp=1;x.btr.nominal.tseg1=5;x.btr.nominal.tseg2=2;x.btr.nominal.sjw=1;x.btr.nominal.sam=0;} while(0)
+#define BITRATE_500K(x)  do {x.btr.frequency=8000000;x.btr.nominal.brp=1;x.btr.nominal.tseg1=13;x.btr.nominal.tseg2=2;x.btr.nominal.sjw=1;x.btr.nominal.sam=0;} while(0)
+#define BITRATE_250K(x)  do {x.btr.frequency=8000000;x.btr.nominal.brp=2;x.btr.nominal.tseg1=13;x.btr.nominal.tseg2=2;x.btr.nominal.sjw=1;x.btr.nominal.sam=0;} while(0)
+#define BITRATE_125K(x)  do {x.btr.frequency=8000000;x.btr.nominal.brp=4;x.btr.nominal.tseg1=13;x.btr.nominal.tseg2=2;x.btr.nominal.sjw=1;x.btr.nominal.sam=0;} while(0)
 
 //  ($5) define macros for unsupported features in CAN Classic operation mode (at least BITRATE_800K_UNSUPPORTED, ..)
 #define BITRATE_800K_UNSUPPORTED  0
-#define BITRATE_5K_UNSUPPORTED  1
-#define TX_ACKNOWLEDGE_UNSUPPORTED  0
+#define BITRATE_5K_UNSUPPORTED  0
+#define TX_ACKNOWLEDGE_UNSUPPORTED  1
+#define STATUS_QUEUE_OVFL_UNSUPPORTED  1
 
 //  (§6) define macros for workarounds in CAN Classic operation mode (e.g. TC01_3_WORKARAOUND)
+#define PCBUSB_INIT_DELAY_WORKAROUND  1
+#define PCBUSB_QXMTFULL_WORKAROUND  1
 //#define TX0x_y_WORKARAOUND  1
 
 //  (§7) define macro CAN_FD_SUPPORTED when CAN FD operation mode is supported
-#define CAN_FD_SUPPORTED  0
+#define CAN_FD_SUPPORTED 1
 #if (CAN_FD_SUPPORTED != 0)
 
 //  (§8) define macros for CAN Classic bit-rate settings (at least BITRATE_1M8M, BITRATE_500K4M, BITRATE_250K2M, BITRATE_125K1M)
-#define BITRATE_1M8M(x)     do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; x.btr.data.brp=0; x.btr.data.tseg1=0; x.btr.data.tseg2=0; x.btr.data.sjw=0; } while(0)
-#define BITRATE_500K4M(x)   do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; x.btr.data.brp=0; x.btr.data.tseg1=0; x.btr.data.tseg2=0; x.btr.data.sjw=0; } while(0)
-#define BITRATE_250K2M(x)   do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; x.btr.data.brp=0; x.btr.data.tseg1=0; x.btr.data.tseg2=0; x.btr.data.sjw=0; } while(0)
-#define BITRATE_125K1M(x)   do{ x.btr.frequency=0; x.btr.nominal.brp=0; x.btr.nominal.tseg1=0; x.btr.nominal.tseg2=0; x.btr.nominal.sjw=0; x.btr.nominal.sam=0; x.btr.data.brp=0; x.btr.data.tseg1=0; x.btr.data.tseg2=0; x.btr.data.sjw=0; } while(0)
+#define BITRATE_1M8M(x)    do {x.btr.frequency=80000000;x.btr.nominal.brp=2;x.btr.nominal.tseg1=31;x.btr.nominal.tseg2=8;x.btr.nominal.sjw=8; \
+                                                        x.btr.data.brp=2;x.btr.data.tseg1=3;x.btr.data.tseg2=1;x.btr.data.sjw=1;} while(0)
+#define BITRATE_500K4M(x)  do {x.btr.frequency=80000000;x.btr.nominal.brp=2;x.btr.nominal.tseg1=63;x.btr.nominal.tseg2=16;x.btr.nominal.sjw=16; \
+                                                        x.btr.data.brp=2;x.btr.data.tseg1=7;x.btr.data.tseg2=2;x.btr.data.sjw=2;} while(0)
+#define BITRATE_250K2M(x)  do {x.btr.frequency=80000000;x.btr.nominal.brp=2;x.btr.nominal.tseg1=127;x.btr.nominal.tseg2=32;x.btr.nominal.sjw=32; \
+                                                        x.btr.data.brp=2;x.btr.data.tseg1=15;x.btr.data.tseg2=4;x.btr.data.sjw=4;} while(0)
+#define BITRATE_125K1M(x)  do {x.btr.frequency=80000000;x.btr.nominal.brp=2;x.btr.nominal.tseg1=255;x.btr.nominal.tseg2=64;x.btr.nominal.sjw=64; \
+                                                        x.btr.data.brp=2;x.btr.data.tseg1=31;x.btr.data.tseg2=8;x.btr.data.sjw=8;} while(0)
 
-//  ($9) define macros for unsupported features in CAN Classic operation mode
+//  ($9) define macros for unsupported features in CAN FD operation mode
 //#define BITRATE_SWITCHING_UNSUPPORTED  0
 
-//  (§10) define macros for workarounds in CAN Classic operation mode (e.g. TC01_3_WORKARAOUND)
+//  (§10) define macros for workarounds in CAN FD operation mode (e.g. TC01_3_WORKARAOUND)
 //#define TX0x_y_WORKARAOUND  1
 
 #endif // CAN_FD_SUPPORTED
