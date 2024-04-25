0a5aaf87cd16208f1b7fe3049509b6c77e003687
Uwe McVogt <mac@uv-software.de>
2024-04-25 12:18:25 +0200

    Update CAN API V3 sources to rev. 1270
    Relates to [MACCAN-191]

--- Sources/CANAPI/can_api.h
+++ Sources/CANAPI/can_api.h
@@ -2,13 +2,13 @@
 /*
  *  CAN Interface API, Version 3 (generic)
  *
- *  Copyright (c) 2004-2023 Uwe Vogt, UV Software, Berlin (info@uv-software.com)
+ *  Copyright (c) 2004-2024 Uwe Vogt, UV Software, Berlin (info@uv-software.com)
  *  All rights reserved.
  *
  *  This file is part of CAN API V3.
  *
- *  CAN API V3 is dual-licensed under the BSD 2-Clause "Simplified" License and
- *  under the GNU General Public License v3.0 (or any later version).
+ *  CAN API V3 is dual-licensed under the BSD 2-Clause "Simplified" License
+ *  and under the GNU General Public License v3.0 (or any later version).
  *  You can choose between one of them if you use this file.
  *
  *  BSD 2-Clause "Simplified" License:
@@ -43,7 +43,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with CAN API V3.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with CAN API V3.  If not, see <https://www.gnu.org/licenses/>.
  */
 /** @file        can_api.h
  *
@@ -51,7 +51,7 @@
  *
  *  @author      $Author: eris $
  *
- *  @version     $Rev: 1090 $
+ *  @version     $Rev: 1270 $
  *
  *  @defgroup    can_api CAN Interface API, Version 3
  *  @{
@@ -136,7 +136,7 @@ typedef int                             can_handle_t;
 #elif (OPTION_CANAPI_DRIVER != 0)
 #define CAN_BOARD(lib, brd)             brd
 #else
-#error Remove the unneeded definition(s)!
+#error Remove the unneeded definition(s)
 #endif
 
 /*  -----------  types  --------------------------------------------------
