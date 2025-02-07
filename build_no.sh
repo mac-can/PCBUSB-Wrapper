1d25ffc56755ccbfd511185fb2657289290bd2f1
Uwe McVogt <mac@uv-software.de>
2025-02-07 19:57:01 +0100

    Update `Makefile`s
    Relates to [MACCAN-228]

--- build_no.sh
+++ build_no.sh
@@ -7,11 +7,11 @@ echo " *  Copyright (c) 2005-2012 Uwe Vogt, UV Software, Friedrichshafen" >> ./S
 echo " *  Copyright (c) 2013-2025 Uwe Vogt, UV Software, Berlin (info@mac-can.com)" >> ./Sources/build_no.h
 echo " *  All rights reserved." >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
-echo " *  This file is part of PCANBasic-Wrapper." >> ./Sources/build_no.h
+echo " *  This file is part of PCBUSB-Wrapper." >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
-echo " *  PCANBasic-Wrapper is dual-licensed under the BSD 2-Clause \"Simplified\" License" >> ./Sources/build_no.h
+echo " *  PCBUSB-Wrapper is dual-licensed under the BSD 2-Clause \"Simplified\" License" >> ./Sources/build_no.h
 echo " *  and under the GNU General Public License v2.0 (or any later version). You can" >> ./Sources/build_no.h
-echo " *  choose between one of them if you use PCANBasic-Wrapper in whole or in part." >> ./Sources/build_no.h
+echo " *  choose between one of them if you use PCBUSB-Wrapper in whole or in part." >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
 echo " *  (1) BSD 2-Clause \"Simplified\" License" >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
@@ -23,7 +23,7 @@ echo " *  2. Redistributions in binary form must reproduce the above copyright n
 echo " *     this list of conditions and the following disclaimer in the documentation" >> ./Sources/build_no.h
 echo " *     and/or other materials provided with the distribution." >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
-echo " *  PCANBasic-Wrapper IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"" >> ./Sources/build_no.h
+echo " *  PCBUSB-Wrapper IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"" >> ./Sources/build_no.h
 echo " *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE" >> ./Sources/build_no.h
 echo " *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE" >> ./Sources/build_no.h
 echo " *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE" >> ./Sources/build_no.h
@@ -32,22 +32,22 @@ echo " *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOOD
 echo " *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER" >> ./Sources/build_no.h
 echo " *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY," >> ./Sources/build_no.h
 echo " *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE" >> ./Sources/build_no.h
-echo " *  OF PCANBasic-Wrapper, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." >> ./Sources/build_no.h
+echo " *  OF PCBUSB-Wrapper, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE." >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
 echo " *  (2) GNU General Public License v2.0 or later" >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
-echo " *  PCANBasic-Wrapper is free software; you can redistribute it and/or modify" >> ./Sources/build_no.h
+echo " *  PCBUSB-Wrapper is free software; you can redistribute it and/or modify" >> ./Sources/build_no.h
 echo " *  it under the terms of the GNU General Public License as published by" >> ./Sources/build_no.h
 echo " *  the Free Software Foundation; either version 2 of the License, or" >> ./Sources/build_no.h
 echo " *  (at your option) any later version." >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
-echo " *  PCANBasic-Wrapper is distributed in the hope that it will be useful," >> ./Sources/build_no.h
+echo " *  PCBUSB-Wrapper is distributed in the hope that it will be useful," >> ./Sources/build_no.h
 echo " *  but WITHOUT ANY WARRANTY; without even the implied warranty of" >> ./Sources/build_no.h
 echo " *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" >> ./Sources/build_no.h
 echo " *  GNU General Public License for more details." >> ./Sources/build_no.h
 echo " *" >> ./Sources/build_no.h
 echo " *  You should have received a copy of the GNU General Public License along" >> ./Sources/build_no.h
-echo " *  with PCANBasic-Wrapper; if not, see <https://www.gnu.org/licenses/>." >> ./Sources/build_no.h
+echo " *  with PCBUSB-Wrapper; if not, see <https://www.gnu.org/licenses/>." >> ./Sources/build_no.h
 echo " */" >> ./Sources/build_no.h
 echo "#ifndef BUILD_NO_H_INCLUDED" >> ./Sources/build_no.h
 echo "#define BUILD_NO_H_INCLUDED" >> ./Sources/build_no.h
