068eccd9b33427fbebdc3431d154e2324bdfa8d7
Uwe McVogt <mac@uv-software.de>
2025-02-17 22:30:23 +0100

    Update CAN/IPC examples
    Relates to [MACCAN-233]

--- Examples/IPC/ipc_send.c
+++ Examples/IPC/ipc_send.c
@@ -1,28 +1,34 @@
 //
 //  ipc_send.c
-//  RocketCAN
+//  RocketCAN CC
 //
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <errno.h>
+#include <time.h>
 #if !defined(_WIN32) && !defined(_WIN64)
 #include <unistd.h>
 #endif
 #include "ipc_can.h"
 #include "ipc_client.h"
+#include "crc_j1850.h"
 
 static void sigterm(int signo);
 static volatile int running = 1;
 
 static const char *server = "localhost:60000";
 
-int main() {
+int main(int argc, char *argv[]) {
     int fildes = (-1);
     can_ipc_message_t msg;
+    struct timespec now;
+    uint32_t i, frames = 2048U;
 
+    if (argc > 1) {
+        frames = (uint32_t)strtoul(argv[1], NULL, 0);
+    }
     if ((signal(SIGINT, sigterm) == SIG_ERR) ||
 #if !defined(_WIN32) && !defined(_WIN64)
         (signal(SIGHUP, sigterm) == SIG_ERR) ||
@@ -37,16 +43,23 @@ int main() {
     }
     printf("Connected to server %s\n", server);
     
-    for (uint32_t i = 0; i < 2048; i++) {
+    printf("Press ^C to abort.\n");
+    for (i = 0; (i < frames) && running; i++) {
         memset(&msg, 0, sizeof(msg));
-        msg.id = i;
+        msg.id = i & 0x7FFU;
         msg.flags = 0;
         msg.length = 8;
         msg.data[0] = (uint8_t)(i & 0xFF);
         msg.data[1] = (uint8_t)((i >> 8) & 0xFF);
         msg.data[2] = (uint8_t)((i >> 16) & 0xFF);
         msg.data[3] = (uint8_t)((i >> 24) & 0xFF);
+        msg.ctrlchar = CANIPC_ETX_CHAR;
+        if (clock_gettime(CLOCK_REALTIME, &now) == 0) {
+            msg.timestamp.tv_sec = now.tv_sec;
+            msg.timestamp.tv_nsec = now.tv_nsec;
+        }
         CAN_IPC_MSG_HTON(msg);
+        msg.checksum = crc_j1850_calc(&msg, sizeof(msg) - 1U, NULL);
         if (ipc_client_send(fildes, (const char*)&msg, sizeof(msg)) < 0) {
             perror("+++ error");
             break;
@@ -56,7 +69,7 @@ int main() {
         perror("+++ error");
         return EXIT_FAILURE;
     }
-    printf("Connection closed\n");
+    printf("Connection closed: %u frames sent\n", i);
     return EXIT_SUCCESS;
 }
 
